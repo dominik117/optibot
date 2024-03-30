c18f21a9a4d6eb7df0166d264f67207c66303f55
Dominik Bacher <dominikbacher@icloud.com>
2024-03-30 19:19:45 +0100

    Syntax finx

--- run_optibot.py
+++ run_optibot.py
@@ -1,10 +1,9 @@
 import streamlit.web.cli as stcli
 import sys
 
+# Shortcut to run streamlit app
 def main():
-    # Modify the argv for Streamlit.
     sys.argv = ["streamlit", "run", "streamlit_app.py"]
-    # Execute Streamlit's main function which starts the app.
     sys.exit(stcli.main())
 
 if __name__ == '__main__':
