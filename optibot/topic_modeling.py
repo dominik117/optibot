4ad3d722652078c8c6f0610363ec22bb3754ac77
Dominik Bacher <dominikbacher@icloud.com>
2024-03-30 18:51:01 +0100

    Cretae class for topic modeling

--- optibot/topic_modeling.py
+++ optibot/topic_modeling.py
@@ -7,9 +7,15 @@ import logging
 import warnings
 import numpy as np
 
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+plt.style.use('fivethirtyeight')
+
 
 # TODO: try with spacy to see if it's faster
-def normalize_corpus(df, conversations):
+def normalize_corpus(conversations):
     stop_words = nltk.corpus.stopwords.words('english')
     wtk = nltk.tokenize.RegexpTokenizer(r'\w+')
     wnl = nltk.stem.wordnet.WordNetLemmatizer()
@@ -29,8 +35,6 @@ def normalize_corpus(df, conversations):
         conversation_tokens = list(filter(None, conversation_tokens))
         if conversation_tokens:
             norm_conversations.append(conversation_tokens)
-
-    norm_conversations = normalize_corpus(df["conversation"].to_list())
 
     return norm_conversations
 
@@ -52,7 +56,7 @@ def gensim_build_bigrams_bow(norm_conversations):
     # Transforming corpus into bag of words vectors:
     bow_corpus = [dictionary.doc2bow(text) for text in norm_conversations_bigrams]
 
-    return bow_corpus, dictionary
+    return bow_corpus, dictionary, norm_conversations_bigrams
 
 def topic_modeling_by_coherence(bow_corpus, conversations, dictionary, start_topic_count=2, end_topic_count=10, step=1):
 
@@ -124,3 +128,59 @@ def topic_modeling_by_coherence(bow_corpus, conversations, dictionary, start_top
     # coherence_plot.show()
 
     return lda_models, coherence_df, coherence_plot
+
+
+class OptiBotTopicModeling:
+    def __init__(self, df):
+        self.df = df
+        self.best_lda_model = None
+        self.coherence_df = None
+        self.coherence_plot = None
+        self.topics_df = None
+        self.corpus_topic_df = None
+        self.bow_corpus = None
+
+    def fit_best_topics(self):
+        norm_conversations = normalize_corpus(self.df["conversation"].to_list())
+        bow_corpus, dictionary, norm_conversations_bigrams = gensim_build_bigrams_bow(norm_conversations)
+        lda_models, coherence_df, coherence_plot = topic_modeling_by_coherence(
+            bow_corpus=bow_corpus, 
+            conversations=norm_conversations_bigrams, 
+            dictionary=dictionary,
+            start_topic_count=3, 
+            end_topic_count=10, 
+            step=1)
+
+        best_model_idx = coherence_df['C_v Score'].idxmax()
+        self.best_lda_model = lda_models[best_model_idx]
+        self.bow_corpus = bow_corpus
+
+    def get_best_topics(self):
+        return None
+
+    def fit_topics_on_data(self):
+        topics = [[(term, round(wt, 3))
+                    for term, wt in self.best_lda_model.show_topic(n, topn=20)]
+                        for n in range(0, self.best_lda_model.num_topics)]
+
+        self.topics_df = pd.DataFrame([', '.join([term for term, wt in topic])
+                                  for topic in topics],
+                             columns=['Terms per Topic'],
+                             index=['Topic'+str(t) for t in range(1, self.best_lda_model.num_topics+1)]
+                             )
+
+        tm_results = self.best_lda_model[self.bow_corpus]
+        corpus_topics = [sorted(topics, key=lambda record: -record[1])[0]
+                            for topics in tm_results]
+
+        self.corpus_topic_df = pd.DataFrame()
+        self.corpus_topic_df['Dominant Topic'] = [item[0]+1 for item in corpus_topics]
+        self.corpus_topic_df['Contribution %'] = [round(item[1]*100, 2) for item in corpus_topics]
+        self.corpus_topic_df['Topic Desc'] = [self.topics_df.iloc[t[0]]['Terms per Topic'] for t in corpus_topics]
+        self.corpus_topic_df['Conversation'] = self.df["conversation"]
+
+        return self.corpus_topic_df
+    
+    def fit_tpics_on_data(self):
+        self.fit_best_topics()
+        return self.get_topics_data()
