b7a82cf14fa29d37477a40b9a1f21c1f897bc9b3
Dominik Bacher <dominikbacher@icloud.com>
2024-03-31 18:30:09 +0200

    Modularize global variables

---
+++ optibot/config.py
@@ -0,0 +1,10 @@
+# Global Configuration for Gensim Bigrams
+MIN_COUNT = 20
+THRESHOLD = 10
+NO_BELOW = 5
+NO_ABOVE = 0.95
+
+# Global Configuration for Gensm LDA
+CHUNKSIZE = 2000 #2000
+ITERATIONS = 100 #100
+PASSES = 20 #20 # epochs
