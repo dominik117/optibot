490101b17be8867bb6e68c0aaeadb81d06b07387
Dominik Bacher <dominikbacher@icloud.com>
2024-04-01 19:25:22 +0200

    Increase iteration for better performance

--- optibot/config.py
+++ optibot/config.py
@@ -5,6 +5,6 @@ NO_BELOW = 5
 NO_ABOVE = 0.95
 
 # Global Configuration for Gensm LDA
-CHUNKSIZE = 1700 #2000
-ITERATIONS = 80 #100
-PASSES = 15 #20 # epochs
+CHUNKSIZE = 2000 #2000
+ITERATIONS = 100 #100
+PASSES = 17 #20 # epochs
