afe1d427be1adfae816a78e4af0aa12224656437
Dominik Bacher <dominikbacher@icloud.com>
2024-04-01 22:11:57 +0200

    Further tunning

--- optibot/config.py
+++ optibot/config.py
@@ -5,6 +5,6 @@ NO_BELOW = 5
 NO_ABOVE = 0.95
 
 # Global Configuration for Gensm LDA
-CHUNKSIZE = 2200 #2000
-ITERATIONS = 90 #100
-PASSES = 17 #20 # epochs
+CHUNKSIZE = 2000 #2000
+ITERATIONS = 10 #100
+PASSES = 20 #20 # epochs
